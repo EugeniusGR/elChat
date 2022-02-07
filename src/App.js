37d3229c4ff21f374a15733273c1027fd903b3c8
Yevhenii Cheremisin <yevhenii.cheremisin@sapient.pro>
2022-02-07 23:40:07 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  const a = 2;
+  return <div>{a}</div>;
+};
+
+export default App;
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-const App = () => {
-  return (
-    <span>Hello</span>
-  )
-}
-
-export default App;
